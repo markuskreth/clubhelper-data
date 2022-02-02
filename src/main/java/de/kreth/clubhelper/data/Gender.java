4c5c38d15de570edacc8e159ff03c7a9c76df217
Markus Kreth <markus.kreth@web.de>
2022-02-02 12:16:51 +0100

    gender serialization fixed

--- src/main/java/de/kreth/clubhelper/data/Gender.java
+++ src/main/java/de/kreth/clubhelper/data/Gender.java
@@ -4,48 +4,46 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum Gender {
 
-    MALE(1), FEMALE(2);
+	MALE(1), FEMALE(2);
 
-    private final int id;
+	private final int id;
 
-    private Gender(int id) {
-	this.id = id;
-    }
-
-    public int getId() {
-	return id;
-    }
-
-    public static Gender valueOf(Integer id) {
-	if (id != null) {
-	    return valueOf(id.intValue());
-	} else {
-	    return null;
+	private Gender(int id) {
+		this.id = id;
 	}
-    }
 
-    @JsonCreator
-    public static Gender valueOf(@JsonProperty("id") int id) {
-	for (Gender g : values()) {
-	    if (g.id == id) {
-		return g;
-	    }
+	public int getId() {
+		return id;
 	}
-	throw new IllegalArgumentException("No Gender for id=" + id + " defined.");
-    }
 
-    public String localized() {
-	switch (this) {
-	case FEMALE:
-	    return "Weiblich";
-	case MALE:
-	    return "MÃ¤nnlich";
-	default:
-	    break;
+	public static Gender valueOf(Integer id) {
+		if (id != null) {
+			return valueOf(id.intValue());
+		} else {
+			return null;
+		}
 	}
-	throw new IllegalStateException("No localized String for " + this);
-    }
+
+	public static Gender valueOf(@JsonProperty("id") int id) {
+		for (Gender g : values()) {
+			if (g.id == id) {
+				return g;
+			}
+		}
+		throw new IllegalArgumentException("No Gender for id=" + id + " defined.");
+	}
+
+	public String localized() {
+		switch (this) {
+		case FEMALE:
+			return "Weiblich";
+		case MALE:
+			return "Männlich";
+		default:
+			break;
+		}
+		throw new IllegalStateException("No localized String for " + this);
+	}
 }
--- src/main/java/de/kreth/clubhelper/data/Person.java
+++ src/main/java/de/kreth/clubhelper/data/Person.java
@@ -10,89 +10,90 @@ import java.util.List;
  */
 public class Person extends BaseEntity implements Serializable {
 
-    private static final long serialVersionUID = -8361264400619997123L;
+	private static final long serialVersionUID = -8361264400619997123L;
 
-    private LocalDate birth;
-    private String prename;
-    private String surname;
+	private LocalDate birth;
+	private String prename;
+	private String surname;
 
-    private Integer gender;
-    private List<GroupDef> groups;
+	private Integer gender;
+	private List<GroupDef> groups;
 
-    public Gender getGender() {
-	return Gender.valueOf(gender);
-    }
-
-    public LocalDate getBirth() {
-	return birth;
-    }
-
-    public void setBirth(LocalDate birth) {
-	this.birth = birth;
-    }
-
-    public String getPrename() {
-	return prename;
-    }
-
-    public void setPrename(String prename) {
-	this.prename = prename;
-    }
-
-    public String getSurname() {
-	return surname;
-    }
-
-    public void setSurname(String surname) {
-	this.surname = surname;
-    }
-
-    public void setGender(Gender gender) {
-	if (gender != null) {
-	    this.gender = gender.getId();
-	} else {
-	    this.gender = null;
+	public Gender getGenderObject() {
+		return Gender.valueOf(gender);
 	}
-    }
 
-    public boolean isMember(GroupDef group) {
-	return groups != null && groups.contains(group);
-    }
-
-    public List<GroupDef> getGroups() {
-	if (groups == null) {
-	    groups = new ArrayList<>();
+	public LocalDate getBirth() {
+		return birth;
 	}
-	return groups;
-    }
 
-    @Override
-    public int hashCode() {
-	final int prime = 59;
-	int result = super.hashCode();
-	result = prime * result;
-	return result;
-    }
+	public void setBirth(LocalDate birth) {
+		this.birth = birth;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (!super.equals(obj))
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	Person other = (Person) obj;
-	if (surname == null) {
-	    if (other.surname != null)
-		return false;
-	} else if (!surname.equals(other.surname))
-	    return false;
-	return true;
-    }
+	public String getPrename() {
+		return prename;
+	}
 
-    @Override
-    public String toString() {
-	return "Person [id=" + getId() + ", prename=" + prename + ", surname=" + surname + "]";
-    }
+	public void setPrename(String prename) {
+		this.prename = prename;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+	
+	public Integer getGender() {
+		return gender;
+	}
+	
+	public boolean isMember(GroupDef group) {
+		return groups != null && groups.contains(group);
+	}
+
+	public List<GroupDef> getGroups() {
+		if (groups == null) {
+			groups = new ArrayList<>();
+		}
+		return groups;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 59;
+		int result = super.hashCode();
+		result = prime * result;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + getId() + ", prename=" + prename + ", surname=" + surname + "]";
+	}
+
 }
